b8f66c201e16988c44739fa8efc6cd69b31c676c
masatomo-ito2 <45160975+masatomo-ito2@users.noreply.github.com>
2019-07-10 14:53:38 +0900

    Update resource.tf

--- resource.tf
+++ resource.tf
@@ -1,6 +1,6 @@
 provider "aws" {
   region      =  "ap-northeast-1"
-  # region = "us-east-1"
+  #  region = "us-east-1"
 }
 
 resource "aws_instance" "masa_tfe" {
@@ -19,6 +19,7 @@ resource "aws_instance" "masa_tfe" {
   tags = {
     Name = "Sentinel demo"
     User = "Masa"
+    TTL = "1h"
   }
 }
 
